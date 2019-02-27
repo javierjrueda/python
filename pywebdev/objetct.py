46a94096197599ee114150d39f2678a360cdc02d
javierjrueda <javier.artkine@gmail.com>
2019-02-27 22:29:57 +0100

    Object oriented part completed

--- pywebdev/objetct.py
+++ pywebdev/objetct.py
@@ -6,6 +6,13 @@ class Program():
 		self.version = float(input("What version?: "))
 		self.skill = input("What skill level?: ")
 
+	
+	def upgrade (self):
+		new_version = float(input("What version?: "))
+		self.version = new_version
+
+		print ("We have updated the version for", self.lang,", to", self.version)
+
 
 p1 = Program()
 
