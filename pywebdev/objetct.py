d04be57fe6ab0602a2b1148effd127cd3c847982
javierjrueda <javier.artkine@gmail.com>
2019-02-27 21:11:16 +0100

    Python web programming course

---
+++ pywebdev/objetct.py
@@ -0,0 +1,12 @@
+class Program():
+        
+	def __init__ (self, *args, **kwargs):
+                
+		self.lang = input("What language?: ")
+		self.version = float(input("What version?: "))
+		self.skill = input("What skill level?: ")
+
+
+p1 = Program()
+
+
