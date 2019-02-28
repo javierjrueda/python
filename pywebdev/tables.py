571a0c2b65baebf04a5c53428dc1852ebe50aa9f
javierjrueda <javier.artkine@gmail.com>
2019-02-28 19:06:02 +0100

    SQLite Intro - part 1

---
+++ pywebdev/tables.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conn = sqlite3.connect('tutorial.db')
+c = conn.cursor()
+
+def create_table():
+    c.execute("CREATE TABLE example(Language VARCHAR, Version REAL, Skill TEXT)")
+
+def enter_data():
+    c.execute("INSERT INTO example VALUES('Python', 2.7, 'Begginer')")
+    c.execute("INSERT INTO example VALUES('Python', 3.3, 'Intermidiate')")
+    c.execute("INSERT INTO example VALUES('Python', 3.4, 'Expert')")
+    conn.commit()
+
+def enter_dynamic_data():
+    lang = input("What language?: ")
+    version = float(input("What version?: "))
+    skill = input("What skill level?: ")
+
+    c.execute("INSERT INTO example (Language, Version, Skill) VALUES(?, ?, ?)", (lang, version, skill))
+    conn.commit()
+
+enter_dynamic_data()
+
+#conn.close()
