1fd3b0ec9989d0280d387abeb579df1818c21050
javierjrueda <javier.artkine@gmail.com>
2019-03-03 21:13:10 +0100

    HTML section completed

--- pywebdev/webscrapping.py
+++ pywebdev/webscrapping.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup 
 import urllib.request
+import time
 
 req = urllib.request.urlopen('https://www.eleconomista.es/rss/rss-flash-del-mercado.php')
 
@@ -10,4 +11,14 @@ xml = BeautifulSoup(req, 'xml')
 
 #Loop for taking all the links of the xml from the third one ([3:])
 for item in xml.findAll('link')[3:]:
-    print(item.text)
+    url = 'http:' + item.text
+    news = urllib.request.urlopen(url).read()
+    print(url)
+#Añades "features = "lxml"" para que limar incompatibilidades entre BeautifulSoup yy lxml
+    page = BeautifulSoup(news, features = "lxml")
+    for p in page.findAll('p'):
+        print(p.text)
+
+        time.sleep(5)
+
+        
