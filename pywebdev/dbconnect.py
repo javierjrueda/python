29509d3c2524792f41a42ffe9a3bfc8392869d84
javierjrueda <javier.artkine@gmail.com>
2019-03-04 20:38:50 +0100

    MySQL with Python in VPS

---
+++ pywebdev/dbconnect.py
@@ -0,0 +1,14 @@
+import MySQLdb
+
+def connection():
+	conn = MySQLdb.connect(host='localhost',
+							user='root',
+							passwd='',
+							db='EpicDB')
+	c = conn.cursor()
+
+	return c, conn
+
+if __name__ == '__main__':
+	c, conn = connection()
+	print('it worked!')
