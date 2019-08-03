8e501bb952db22530b7b7648c2b279bcac02c21d
javierjrueda <43324374+javierjrueda@users.noreply.github.com>
2019-08-03 20:56:00 +0200

    Update 2_minion_game.py

--- Hackerrank.com/2_minion_game.py
+++ Hackerrank.com/2_minion_game.py
@@ -1,26 +1,46 @@
 def minion_game(string):
-    mb = "Stuart"
-    ma = "Kevin"
-    vocal = ['a','e','i','o','u']
+    #variables para el bucle de substrings
+    i = 0
+    f = 1
+    l = len(string)
+    scoreb = 0
+    scorea = 0
+
+    #Generación de substrings
+    while i < l+1:
+        while f < l+1:
+            substring = string[i:f]
+            
+            
+            if len(substring) > 0:
+                first = substring[0]
+
+                #Detectamos si la primera letra de la substring es vocal o consonante
+                if first in ('AEIOU'):
+                    scorea += 1
+                    print (substring)
+                    print (txta.format(scorea)) 
+                
+                else:
+                    scoreb += 1
+                    txtb = "Stuart {}"
+                    print (substring)
+                    print (txtb.format(scoreb))
+                f += 1
+            else:
+                f += 1
+        
+        #Reiniciamos la variable f y sumamos la variable i
+        f = 1
+        i += 1 
     
-    #Generamos la primera substring
-    substring = string[0]
-    word = substring.lower()
-    
-    #Detectamos si la primera letra de la substring es vocal o consonante
-    first = word[0]
-    if first == "a" or "e" or "i" or "o" or "u": 
-        score = string.count(word)
-        print (score)
+    if scorea > scoreb:
+        txta = "Kevin {}"
+        print (txta.format(scorea))
     else:
-        print ("Consonant")
-
-    #len()
-    #startswith()
-    #strip()
-    #count()
-
-
+        txtb = "Stuart {}"
+        print (txtb.format(scoreb))
+ 
 
 if __name__ == '__main__':
     s = input()
