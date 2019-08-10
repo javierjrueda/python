c96e7fb22bfa94214d071c801b941df365862d12
Javier J Rueda <43324374+javierjrueda@users.noreply.github.com>
2019-08-10 20:07:16 +0200

    Logo challenge solved!

--- Hackerrank.com/4_logo.py
+++ Hackerrank.com/4_logo.py
@@ -12,23 +12,33 @@ scoredict = {
     "reps" : "",
 }
 
+def myFunc(e):
+  return e["letter"]
+
+def myFuncreps(e):
+  return e["reps"]
+
 def logo(s):
-    abc = 'abdefghijklmnopqrstuvwxyz'
+    abc = 'abcdefghijklmnopqrstuvwxyz'
 
     for i in abc:
         reps = s.count(i)
-        scorelist.append(
-            scoredict={
+        scoredict={
             "letter" : i,
-            "reps" : reps})
-    scorelist.sort(key="letter")
-    scorelist.sort(reverse=True , key="reps")
+            "reps" : reps}
+        scorelist.append(scoredict)
+            
+    scorelist.sort(key=myFunc)
+    scorelist.sort(reverse=True , key=myFuncreps)
 
-    print(scorelist[1])
-    print(scorelist[2])
-    print(scorelist[3])
-
-
+    i = 0
+    while i < 3:
+        windict = scorelist[i]
+        x = windict.get("letter")
+        y = windict.get("reps")
+        
+        print("{} {}".format(x, y))
+        i += 1
 
 
 if __name__ == '__main__':
